7740891557eabf6add4addd86087a8bc0bb82bd6
sofia-garcia30 <sofiamarcelo2516@gmail.com>
2026-04-07 04:45:08 -0500

    dockerizacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://backend-en-railway.up.railway.app',
   cloudinary: {
     cloudName: 'dnipsykly',
     uploadPreset: 'Bike-Shop'
