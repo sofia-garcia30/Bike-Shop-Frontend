1f8aacd97e38bbee901256485aee340fd0e50ce3
sofia-garcia30 <sofiamarcelo2516@gmail.com>
2026-04-07 16:41:25 -0500

    arreglos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend-en-railway.up.railway.app',
+  apiUrl: 'https://proactive-fulfillment-production-f1e7.up.railway.app',
   cloudinary: {
     cloudName: 'dnipsykly',
     uploadPreset: 'Bike-Shop'
