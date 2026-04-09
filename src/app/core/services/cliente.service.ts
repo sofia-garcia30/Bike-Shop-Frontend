a1d6c83aa161061cc2713684973774d5eaa76197
sofia-garcia30 <sofiamarcelo2516@gmail.com>
2026-04-08 23:19:31 -0500

    rutas

--- src/app/core/services/cliente.service.ts
+++ src/app/core/services/cliente.service.ts
@@ -1,1 +1,14 @@
-c
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class ClienteService {
+  private http = inject(HttpClient);
+  private api = environment.apiUrl;
+
+  listar(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.api}/api/clientes`);
+  }
+}
